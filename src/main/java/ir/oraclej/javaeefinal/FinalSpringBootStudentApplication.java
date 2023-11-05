354eeae512e6491a15ac2ba678ca284320d2c9b9
Arash Hemmati <a.hemmati@gmail.com>
2023-11-05 18:48:00 +0330

    Initial commit

---
+++ src/main/java/ir/oraclej/javaeefinal/FinalSpringBootStudentApplication.java
@@ -0,0 +1,13 @@
+package ir.oraclej.javaeefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSpringBootStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalSpringBootStudentApplication.class, args);
+	}
+
+}
